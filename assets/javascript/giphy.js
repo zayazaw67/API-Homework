09d646a932a4e91706adca1406ced1cbee271b0c
Zaya <zayazaw67@gmail.com>
2019-09-17 23:16:18 -0500

    added attr and class to images, added click to pause and play gifs

--- assets/javascript/giphy.js
+++ assets/javascript/giphy.js
@@ -1,36 +1,61 @@
-let topics = ["overwatch", "cats", "warcraft", "ocean"]
+var topics = ["overwatch", "cats", "warcraft", "ocean"];
 
-// let apiKey = pc2wgwNui7jyP6rmS59xhw4oBClWtnID
+// var apiKey = pc2wgwNui7jyP6rmS59xhw4oBClWtnID
 
 function displayTopics() {
+    var search = $(this).attr("data-topic");
 
-    let search = $(this).attr("search-name");
-    let queryURL = "https://api.giphy.com/v1/gifs/search?api_key=" + apiKey;
+    var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=pc2wgwNui7jyP6rmS59xhw4oBClWtnID&q=" + search + "&limit=10&offset=0&rating=PG&lang=en"
+    console.log('url we are about ot hit', queryURL)
 
     $.ajax({
         url: queryURL,
         method: "GET"
     }).then(function (response) {
-        let topicDiv = $("<div class='topic")
-        let rating = response.rating
-        let pRating = $("<p>").text("Rating: " + rating);
-        topicDiv.append(pRating);
+        console.log(response)
+        for (var i = 0; i < response.data.length; i++) {
+            // create new div to store gifs
+            var topicDiv = $("<div>");
+            var pic = $('<img>');
+            var picStill = response.data[i].images.fixed_height_still.url;
+            var picAnimate = response.data[i].images.fixed_height.url;
+            // applying attributes to the images to manipulate later
+            pic.attr('data-still', picStill);
+            pic.attr('data-animate', picAnimate);
+            pic.attr('src', picStill);
+            // adding class to target manipuation
+            pic.attr('class', "topic");
+            pic.attr('data-state', "still")
+            // grabbing rating for images
+            var type = response.data[i].rating;
+            var pType = $("<p>").text("rating :" + type);
+            topicDiv.append(pType, pic);
 
-        $(".gif-dump").prepend(topicDiv);
+            $(".gif-dump").prepend(topicDiv);
+        }
+
+    })
+};
+
+$(document).on("click", ".topic", function () {
+    var state = $(this).attr("data-state");
+    if (state === "still") {
+        $(this).attr("src", $(this).attr("data-animate"));
+        $(this).attr("data-state", "animate");
+    } else {
+        $(this).attr("src", $(this).attr("data-still"));
+        $(this).attr("data-state", "still");
     }
-    )
-}
-
+});
 
 function showButtons() {
-    // delete movies before adding more
     $(".buttons").empty();
     // loop array
-    for (let i = 0; i < topics.length; i++) {
-        let a = $("<button>")
+    for (var i = 0; i < topics.length; i++) {
+        var a = $("<button>")
         // add button class
         a.addClass("topics-button");
-        a.attr("search-name"), topics[i]
+        a.attr("data-topic", topics[i])
         // add topics text to button
         a.text(topics[i]);
         // appends to buttons class
@@ -38,25 +63,17 @@ function showButtons() {
     }
 }
 
-$("#add-topic").on("click", function(event) {
+$("#add-topic").on("click", function (event) {
     event.preventDefault();
     // input from text
-    let searchedTopic = $("#searched-topics").val().trim();
+    var searchedTopic = $("#searched-topics").val().trim();
     // push into array
     topics.push(searchedTopic);
     // calls function to create buttons for input
     showButtons();
 });
 
-// $(document).on("click", ".topics-btn", displayTopics);
+$(document).on("click", ".topics-button", displayTopics);
 
 showButtons();
 
-
-// add into displayTopics later
-        // let rating = response.rating
-        // let pRating = $("<p>").text("Rating: " + rating);
-        // topicDiv.append(pRating);
-        // let rating = response.rating
-        // let pRating = $("<p>").text("Rating: " + rating);
-        // topicDiv.append(pRating);
